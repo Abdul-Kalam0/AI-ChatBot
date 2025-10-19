7709125466548a72075446ac73f6746db60ef085
Abdul-Kalam0 <abdulkalamblycomp@gmail.com>
2025-10-19 11:19:27 +0530

    fix(cors): Update frontend URL in CORS

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ import chatRoutes from "./routes/chatRoutes.js";
 app.use(express.json());
 app.use(
   cors({
-    origin: "https://ai-chatbot001.vercel.app/",
+    origin: "https://ai-chatbot001.vercel.app",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
