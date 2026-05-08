835ae00a33aac0f66a8c3c9c49477df6433e4e22
Abdul-Kalam0 <abdulkalamblycomp@gmail.com>
2026-05-08 11:35:02 +0530

    CORS config

--- server/index.js
+++ server/index.js
@@ -42,7 +42,7 @@ app.use(
 app.use("/api", chatRoutes);
 
 app.get("/", (req, res) => {
-  res.json(<h1>PrepAI backend running✅</h1>);
+  res.send(<h1>PrepAI backend running✅</h1>);
 });
 
 export default app;
