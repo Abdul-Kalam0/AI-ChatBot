4eb4f4b7da8e4be1819ac6def97a2a566bdd9c11
Abdul-Kalam0 <abdulkalamblycomp@gmail.com>
2026-05-17 17:55:32 +0530

    CORS config

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -96,8 +96,8 @@ export const login = async (req, res) => {
 
     res.cookie("token", token, {
       httpOnly: true,
-      secure: false,
-      sameSite: "strict",
+      secure: true,
+      sameSite: "none",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
@@ -124,8 +124,8 @@ export const logout = async (req, res) => {
   try {
     res.clearCookie("token", {
       httpOnly: true,
-      secure: false,
-      sameSite: "strict",
+      secure: true,
+      sameSite: "none",
     });
     return res.status(200).json({
       success: true,
